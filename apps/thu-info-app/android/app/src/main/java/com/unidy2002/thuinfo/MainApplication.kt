bcfcfa18bf570a8837f88fd40ca3e6e2ed162bdc
Thomas Liu <guotaoliu2026@gmail.com>
2026-08-21 11:47:06 +0800

    chore(android): allow custom source build

--- apps/thu-info-app/android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
+++ apps/thu-info-app/android/app/src/main/java/com/unidy2002/thuinfo/MainApplication.kt
@@ -23,8 +23,6 @@ class MainApplication : Application(), ReactApplication {
     override fun onCreate() {
         super.onCreate()
         loadReactNative(this)
-        preventRoot()
         preventEmulator()
-        verifySignature()
     }
 }
